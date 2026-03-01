c68258d574e6f7bffd0cb4a9185d82b3bc98b542
melbinjp <91303803+melbinjp@users.noreply.github.com>
2026-03-01 16:55:10 +0000

    Code Health: Improve Animation Loop Performance by binding this.animate

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ class ModelViewer {
             close: `<svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 6L6 18" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M6 6L18 18" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`
         };
         
+        this.animate = this.animate.bind(this);
         this.init();
         this.setupEventListeners();
         this.animate();
@@ -551,7 +552,7 @@ class ModelViewer {
     }
 
     animate() {
-        requestAnimationFrame(() => this.animate());
+        requestAnimationFrame(this.animate);
         const delta = this.clock.getDelta();
 
         if (!this.superheroMode) {
